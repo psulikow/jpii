1094f0480c2f343eea2f44590221a748581a70d8
Paul Sulikowski <psulikow@gmail.com>
2020-06-19 13:59:46 -0400

    Working as planned just need to fix warnings and segue to fit window in iPad

---
+++ reflections/TextFieldsFont.swift
@@ -0,0 +1,34 @@
+import Foundation
+import UIKit
+
+extension UILabel {
+    
+    open override func awakeFromNib() {
+        self.font = self.font.withSize(self.font.pointSize.relativeToIphone8Width())
+    }
+    
+}
+
+extension UITextView {
+    
+    open override func awakeFromNib() {
+        self.font = self.font?.withSize((self.font?.pointSize.relativeToIphone8Width())!)
+    }
+    
+}
+
+extension UITextField {
+    
+    open override func awakeFromNib() {
+        self.font = self.font?.withSize((self.font?.pointSize.relativeToIphone8Width())!)
+    }
+    
+}
+
+extension UIButton {
+    
+    open override func awakeFromNib() {
+        self.titleLabel?.font = self.titleLabel?.font.withSize((self.titleLabel?.font.pointSize.relativeToIphone8Width())!)
+    }
+    
+}
