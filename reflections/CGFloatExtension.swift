385ae46c418fc9a11ae2e7d8907a914d3bf1b48e
Paul Sulikowski <psulikow@gmail.com>
2020-06-19 14:29:38 -0400

    removed keywindow and changed build to 2

--- reflections/CGFloatExtension.swift
+++ reflections/CGFloatExtension.swift
@@ -18,11 +18,7 @@ extension CGFloat {
     }
     
     func relativeToIphone8Height(shouldUseLimit: Bool = true) -> CGFloat {
-        var extraHeight: CGFloat = 0
-        if #available(iOS 11.0, *) {
-            extraHeight = UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
-            extraHeight = extraHeight + (UIApplication.shared.keyWindow?.safeAreaInsets.top ?? 20) - 20
-        }
+        let extraHeight: CGFloat = 0
         let upperScaleLimit: CGFloat = 1.8
         var toUpdateValue = floor(self * ((UIScreen.main.bounds.height - extraHeight) / 667))
         guard self > minScalableValue else {return toUpdateValue}
